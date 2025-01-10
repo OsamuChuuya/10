d906c8ef8ab4ac429f1a5626d9f6d2fd5c184b9a
OsamuChuuya <osamuchuuya26@gmail.com>
2025-01-10 15:59:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor="blue"
+let imya = prompt(`Имя`)
+let bye = document.createElement("h1")
+bye.textContent=(`${imya}`)
+document.body.appendChild(bye)
+bye.style.color="yellow"
+bye.style.textAlign="center"
+bye.style.marginTop="170px"
+
